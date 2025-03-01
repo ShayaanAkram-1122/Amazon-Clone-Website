784398c7469e8827e978e313390c8f674e4fa2ce
Shayaan <shayaanakram372@gmail.com>
2025-03-01 12:41:17 +0500

    Making the paymentSummary and checkout files

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,2 +1,4 @@
-import { renderCart } from "./checkout/orderSummary.js";
+import { renderCart } from "./checkout/orderSummary.js"; 
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 renderCart();
+renderPaymentSummary();
