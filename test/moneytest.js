f30fe843f4531b889cb3d153f28ad46a542ee96f
Shayaan <shayaanakram372@gmail.com>
2025-03-03 20:22:20 +0500

    Create automated Tests for format Currency

---
+++ test/moneytest.js
@@ -0,0 +1,16 @@
+import { formatCurrency }from "../scripts/utils/money.js";
+
+console.log('TEST SUITE');
+if (formatCurrency(2095) === '20.95') {
+    console.log('passed');
+}
+else
+{
+    console.log('failed');
+}
+
+
+if(formatCurrency(2000.5)==='20.01')
+{
+    console.log('Clear');
+}
