2095f88f880aea886d4b073ed5289350ea5fc309
Shayaan <shayaanakram372@gmail.com>
2025-03-16 16:33:51 +0500

    Making request to backEnds

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,5 @@
 import { renderCart } from "./checkout/orderSummary.js"; 
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import '../data/back-end.js';
 renderCart();
 renderPaymentSummary();
